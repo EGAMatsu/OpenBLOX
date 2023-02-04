2d32766ec1f6fda547c19738df858effe3a7481d
bluepilledgreat <97983689+bluepilledgreat@users.noreply.github.com>
2023-02-04 11:56:48 +0000

    Delete build directory

--- build/tonccpy.d
+++
@@ -1,5 +0,0 @@
-tonccpy.o: C:/Users/Luke/Documents/OpenBLOX/OpenBLOX/source/tonccpy.c \
- C:/Users/Luke/Documents/OpenBLOX/OpenBLOX/include/tonccpy.h \
- E:/devkitPro/libnds/include/nds/ndstypes.h
-C:/Users/Luke/Documents/OpenBLOX/OpenBLOX/include/tonccpy.h:
-E:/devkitPro/libnds/include/nds/ndstypes.h:
