7da9c8a895c70130814a6143e697731558147fda
EGAMatsu <theminingboyalpha@outlook.com>
2024-11-29 15:12:59 -0600

    Add some defines for various runtime modes (Minigame, Solo, Online, Studio)

--- C_Engine/CoreEngine/gameGlobalVariables.h
+++ C_Engine/CoreEngine/gameGlobalVariables.h
@@ -5,4 +5,11 @@
 unsigned char isGameRunning = 1;
 unsigned char isDebgRunning = 0;
 
+#define MINIGAME_MODE 0
+#define PLAYSOLO_MODE 1
+#define ONLINEPL_MODE 2
+#define __STUDIO_MODE 3
+
+unsigned char runtimeMode = 0
+
 void gameRenderLoop();
--- C_Engine/main.c
+++ C_Engine/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+// Engine core that should be defined before the core platform includes
 #include "CoreEngine/gameGlobalVariables.h"
 #include "CoreEngine/color.h"
 #include "CoreEngine/timing.h"
 
+// Third party pre-platform includes
 #include "ThirdParty/TinyPhysicsEngine/tinyphysicsengine.h"
 
 #ifdef NDS_BUILD
@@ -12,8 +14,8 @@
     #include "Platform/PC/pcINC.h"
 #endif
 
+// Post platform includes
 #include "CoreEngine/GL/gameGL_globals.h"
-
 
 int main()
 {
