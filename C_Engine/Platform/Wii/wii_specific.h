e4b92c1afb484e47c381469af3852bd9f6f5d37e
EGAMatsu <theminingboyalpha@outlook.com>
2025-03-03 04:41:59 -0600

    Readd missing variables Wii/RVL.

--- C_Engine/Platform/Wii/wii_specific.h
+++ C_Engine/Platform/Wii/wii_specific.h
@@ -7,10 +7,13 @@
 #include <gccore.h>
 #include <fat.h>
 
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
+
 void wiiInit() {
     // Initialise the video system
 	VIDEO_Init();
-	
+
 	// Obtain the preferred video mode from the system
 	// This will correspond to the settings in the Wii menu
 	rmode = VIDEO_GetPreferredMode(NULL);
