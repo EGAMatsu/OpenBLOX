7b785cad7b7dcfa3bc94d8129542851108f70cf7
EGAMatsu <theminingboyalpha@outlook.com>
2025-03-03 04:40:40 -0600

    Readd console for Wii/RVL.

--- C_Engine/Platform/Wii/wii_specific.h
+++ C_Engine/Platform/Wii/wii_specific.h
@@ -7,17 +7,35 @@
 #include <gccore.h>
 #include <fat.h>
 
-static void *xfb = NULL;
-static GXRModeObj *rmode = NULL;
 void wiiInit() {
     // Initialise the video system
 	VIDEO_Init();
+
+	// This function initialises the attached controllers
+	WPAD_Init();
 
 	// Obtain the preferred video mode from the system
 	// This will correspond to the settings in the Wii menu
 	rmode = VIDEO_GetPreferredMode(NULL);
 
+	// Allocate memory for the display in the uncached region
+	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+
+	// Initialise the console, required for printf
+	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+
+	// Set up the video registers with the chosen mode
 	VIDEO_Configure(rmode);
 
+	// Tell the video hardware where our display memory is
+	VIDEO_SetNextFramebuffer(xfb);
+
+	// Make the display visible
+	VIDEO_SetBlack(false);
+
+	// Flush the video register changes to the hardware
+	VIDEO_Flush();
+
+	// Wait for Video setup to complete
 	VIDEO_WaitVSync();
 }
