c6dd5458a007f47865d9691f7a0e4121504b5ebe
EGAMatsu <theminingboyalpha@outlook.com>
2025-03-03 16:07:49 -0600

    Wii no longer crashes, no image on screen.

--- C_Engine/Platform/Wii/wii_specific.h
+++ C_Engine/Platform/Wii/wii_specific.h
@@ -13,13 +13,15 @@ static void *xfb = NULL;
 static GXRModeObj *rmode = NULL;
 
 void wiiInit() {
-	VIDEO_Init();
-	rmode = VIDEO_GetPreferredMode(NULL);
-	xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-	console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
-	VIDEO_Configure(rmode);
-	VIDEO_SetNextFramebuffer(xfb);
-	VIDEO_SetBlack(false);
-	VIDEO_Flush();
-	VIDEO_WaitVSync();
+	#ifdef WII_CONSOLE
+		VIDEO_Init();
+		rmode = VIDEO_GetPreferredMode(NULL);
+		xfb = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+		console_init(xfb,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+		VIDEO_Configure(rmode);
+		VIDEO_SetNextFramebuffer(xfb);
+		VIDEO_SetBlack(false);
+		VIDEO_Flush();
+		VIDEO_WaitVSync();
+	#endif
 }
